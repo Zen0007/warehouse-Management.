11e30746cd76aff1dd9040f72d1d1c95b2e51114
Asta-2007 <docker624@gmail.com>
2025-01-13 14:59:21 +0700

    add brige betwen first page and detail borrow stuff user

---
+++ lib/page/middle_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:werehouse_inventory/page/first_screen.dart';
+import 'package:werehouse_inventory/screeen/user_has_borrow.dart';
+import 'package:werehouse_inventory/shered_data_to_root/websocket_helper.dart';
+
+class MiddlePage extends StatefulWidget {
+  const MiddlePage({super.key});
+
+  @override
+  State<MiddlePage> createState() => _MiddlePageState();
+}
+
+class _MiddlePageState extends State<MiddlePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Consumer<WebsocketHelper>(
+        builder: (context, wsHelper, child) {
+          return StreamBuilder(
+            stream: wsHelper.checkUserHasBorrow(),
+            builder: (context, snapshot) {
+              print("${snapshot.data} data middle");
+              if (snapshot.hasData) {
+                return const UserHasBorrows();
+              } else {
+                return const FirstScreen();
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
