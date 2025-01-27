183374cca5c8a797d674126f885c8df7a04fd65b
Zen0007 <zmr8388@gmail.com>
2025-01-27 11:41:54 +0700

    improve stucture folder

---
+++ lib/screeen/admin_stuff/borrow.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:werehouse_inventory/card/card_borrow.dart';
+import 'package:werehouse_inventory/shered_data_to_root/websocket_helper.dart';
+
+class BorrowUserPage extends StatelessWidget {
+  const BorrowUserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: Text(
+          "Daftar  Peminjam",
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Consumer<WebsocketHelper>(
+        builder: (contex, wsHelper, child) {
+          //  this code fro catch data from borrow user
+          wsHelper.getDataBorrow(); // listner database
+
+          return StreamBuilder(
+            stream: wsHelper.borrowUser(),
+            builder: (context, snapshot) {
+              debugPrint("${snapshot.data} is exist");
+              debugPrint(
+                  "${snapshot.connectionState == ConnectionState.waiting}  has data");
+
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator.adaptive(),
+                );
+              } else if (snapshot.hasData) {
+                if (snapshot.data!.isEmpty) {
+                  return Center(
+                    child: Text(
+                      'data is empty',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                    ),
+                  );
+                }
+                return LayoutBuilder(
+                  builder: (context, constraints) {
+                    int count;
+                    double mainAxisExtent;
+                    debugPrint("${constraints.maxWidth} size");
+                    if (constraints.maxWidth < 400) {
+                      count = 1;
+                      mainAxisExtent = constraints.maxHeight * 1.4;
+                    } else if (constraints.maxWidth < 500) {
+                      count = 2;
+                      mainAxisExtent = constraints.maxWidth * 0.66;
+                    } else if (constraints.maxWidth < 700) {
+                      count = 3;
+                      mainAxisExtent = constraints.maxWidth * 0.47;
+                    } else if (constraints.maxWidth < 900) {
+                      count = 4;
+                      mainAxisExtent = constraints.maxWidth * 0.35;
+                    } else if (constraints.maxWidth < 1000) {
+                      count = 5;
+                      mainAxisExtent = constraints.maxWidth * 0.3;
+                    } else {
+                      count = 6;
+                      mainAxisExtent = constraints.maxWidth * 0.25;
+                    }
+
+                    double sizeImage = constraints.maxWidth / count;
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      padding: const EdgeInsets.all(10),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: count,
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 1,
+                        mainAxisExtent: mainAxisExtent,
+                      ),
+                      itemBuilder: (context, index) {
+                        return CardBorrow(
+                          data: snapshot.data![index],
+                          imageSize: sizeImage,
+                        );
+                      },
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                debugPrint("${snapshot.error}");
+                return Center(
+                  child: SizedBox(
+                    width: size.width * 04,
+                    height: size.height * 0.5,
+                    child: Card(
+                      color: Theme.of(context).colorScheme.surface,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 4,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            title: Text(
+                              "${snapshot.error}",
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }
+
+              return Center(
+                child: SizedBox(
+                  width: size.width * 0.4,
+                  height: size.height * 0.4,
+                  child: Card(
+                    color: Theme.of(context).colorScheme.surface,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 4,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text("${snapshot.error}"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
