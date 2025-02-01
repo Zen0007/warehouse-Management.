9fb9a32d70ed64a6c94c94d793a44b9b6df98046
Zen0007 <zmr8388@gmail.com>
2025-02-01 14:18:00 +0700

    progress test page for admin

--- lib/page/app.dart
+++ lib/page/app.dart
@@ -4,6 +4,7 @@ import 'package:werehouse_inventory/page/home_page.dart';
 import 'package:werehouse_inventory/page/middle_page.dart';
 import 'package:werehouse_inventory/theme_data/theme_page.dart';
 import 'package:werehouse_inventory/shered_data_to_root/websocket_helper.dart';
+import 'package:werehouse_inventory/configuration/delete/controler_service_deleted.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -15,27 +16,28 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: ThemeColorPage.lightTheme,
       darkTheme: ThemeColorPage.darkTheme,
-      home: Consumer<WebsocketHelper>(
-        builder: (context, wsHelper, child) {
-          /*
-          check token user in local storage
-          */
-          return StreamBuilder(
-            stream: wsHelper.verifikasi(),
-            builder: (context, snapshot) {
-              // print('${snapshot.data} status');
-              /*
-               check if token user is valide else is token invalide and nothing return
-              */
-              if (snapshot.hasData) {
-                return HomePage();
-              } else {
-                return const MiddlePage();
-              }
-            },
-          );
-        },
-      ),
+      home: HomePage(),
+      // home: Consumer<WebsocketHelper>(
+      //   builder: (context, wsHelper, child) {
+      //     /*
+      //     check token user in local storage
+      //     */
+      //     return StreamBuilder(
+      //       stream: wsHelper.verifikasi(),
+      //       builder: (context, snapshot) {
+      //         // print('${snapshot.data} status');
+      //         /*
+      //          check if token user is valide else is token invalide and nothing return
+      //         */
+      //         if (snapshot.hasData) {
+      //           return HomePage();
+      //         } else {
+      //           return const MiddlePage();
+      //         }
+      //       },
+      //     );
+      //   },
+      // ),
     );
   }
 }
