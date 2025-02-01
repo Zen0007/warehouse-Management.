fd6a5d439621b6778e3916f130e7ca3897e9ce96
Zen0007 <zmr8388@gmail.com>
2025-02-01 14:32:11 +0700

    update some function

--- lib/screeen/user_stuff/list_key_category.dart
+++ lib/screeen/user_stuff/list_key_category.dart
@@ -1,30 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:werehouse_inventory/screeen/user_stuff/each_category.dart';
+import 'package:werehouse_inventory/screeen/user_stuff/category_grid_item.dart';
+import 'package:werehouse_inventory/screeen/user_stuff/list_stuffs_user.dart';
+import 'package:werehouse_inventory/shered_data_to_root/websocket_helper.dart';
 
-// grid item
-class ListCategory extends StatelessWidget {
-  const ListCategory({
-    super.key,
-    required this.category,
-    required this.onSelectCategory,
-  });
+class ListCategoryUser extends StatelessWidget {
+  const ListCategoryUser({super.key});
 
-  final String category;
-  final void Function() onSelectCategory;
+  void selectCategory(BuildContext context, String title) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ScreenCategoryUser(
+          title: title,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(
-        color: Theme.of(context).colorScheme.onPrimary,
-        Icons.circle_outlined,
-      ),
-      title: Text(
-        category,
-        style: TextStyle(
-          color: Theme.of(context).colorScheme.onPrimary,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: Text(
+          "List Category",
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
         ),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ListStaffUser(),
+              ),
+            ),
+            style: TextButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.surface,
+            ),
+            icon: Icon(
+              Icons.bookmark,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+        ],
       ),
-      onTap: onSelectCategory,
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Consumer<WebsocketHelper>(
+        builder: (contex, wsHelper, child) {
+          wsHelper.getAllKeyCategory(); // listen change database
+
+          return StreamBuilder(
+            stream: wsHelper.keyCategory().asStream(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                );
+              } else if (snapshot.data!.isEmpty) {
+                return Center(
+                  child: Text(
+                    'data is empty',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                );
+              }
+              if (snapshot.hasData) {
+                return LayoutBuilder(
+                  builder: (context, constraints) {
+                    int count;
+                    double mainAxisExtent;
+                    debugPrint("${constraints.maxWidth} WITHT");
+
+                    if (constraints.maxWidth < 480) {
+                      count = 2;
+                      mainAxisExtent = constraints.maxWidth * 0.4;
+                    } else if (constraints.maxWidth < 700) {
+                      count = 3;
+                      mainAxisExtent = constraints.maxWidth * 0.25;
+                    } else if (constraints.maxWidth < 900) {
+                      count = 4;
+                      mainAxisExtent = constraints.maxWidth * 0.2;
+                    } else if (constraints.maxWidth < 1000) {
+                      count = 5;
+                      mainAxisExtent = constraints.maxWidth * 0.17;
+                    } else {
+                      count = 6;
+                      mainAxisExtent = constraints.maxWidth * 0.15;
+                    }
+
+                    // double sizeImage = constraints.maxWidth / count;
+
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      padding: const EdgeInsets.all(20),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: count,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 8,
+                        mainAxisExtent: mainAxisExtent,
+                      ),
+                      itemBuilder: (context, index) {
+                        return CategoryGridItem(
+                          title: snapshot.data![index],
+                          onSelectCategory: () {
+                            wsHelper
+                                .getDataCategoryUserOnce(); // triger ws send data
+                            selectCategory(
+                              context,
+                              snapshot.data![index].key,
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
+                );
+              } else {
+                return Center(
+                  child: Text(
+                    'is not found ${snapshot.error}',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                );
+              }
+            },
+          );
+        },
+      ),
     );
   }
 }
