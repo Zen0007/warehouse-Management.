384695cbfd730889d7047cf02bc96126bbabbf9e
Zen0007 <zmr8388@gmail.com>
2025-02-01 14:26:35 +0700

    improve hendel erro in stream

--- lib/screeen/admin_stuff/category_admin.dart
+++ lib/screeen/admin_stuff/category_admin.dart
@@ -3,17 +3,21 @@ import 'package:provider/provider.dart';
 import 'package:werehouse_inventory/card/card_item.dart';
 import 'package:werehouse_inventory/shered_data_to_root/websocket_helper.dart';
 
-class ScreenCategoryAdmin extends StatelessWidget {
-  const ScreenCategoryAdmin({super.key, required this.title});
+class CategoryAdmin extends StatefulWidget {
+  const CategoryAdmin({super.key, required this.title});
   final String title;
+  @override
+  State<CategoryAdmin> createState() => _CategoryAdminState();
+}
 
+class _CategoryAdminState extends State<CategoryAdmin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.surface,
         title: Text(
-          title,
+          widget.title,
           style: TextStyle(
             color: Theme.of(context).colorScheme.onPrimary,
           ),
@@ -33,71 +37,75 @@ class ScreenCategoryAdmin extends StatelessWidget {
           wsHelper.getDataAllCollection();
 
           return StreamBuilder(
-            stream: wsHelper.indexCategoryForAdmin(title),
+            stream: wsHelper.indexCategoryForAdmin(widget.title),
             builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                if (snapshot.data!.isNotEmpty) {
-                  return LayoutBuilder(
-                    builder: (context, constraints) {
-                      int count;
-                      double mainAxisExtent;
-                      print(constraints.maxWidth);
-
-                      if (constraints.maxWidth < 480) {
-                        count = 2;
-                        mainAxisExtent = constraints.maxWidth * 0.67;
-                      } else if (constraints.maxWidth < 700) {
-                        count = 3;
-                        mainAxisExtent = constraints.maxWidth * 0.5;
-                      } else if (constraints.maxWidth < 900) {
-                        count = 4;
-                        mainAxisExtent = constraints.maxWidth * 0.35;
-                      } else if (constraints.maxWidth < 1000) {
-                        count = 5;
-                        mainAxisExtent = constraints.maxWidth * 0.3;
-                      } else {
-                        count = 6;
-                        mainAxisExtent = constraints.maxWidth * 0.25;
-                      }
-
-                      double sizeImage = constraints.maxWidth / count;
-                      return GridView.builder(
-                        shrinkWrap: true,
-                        itemCount: snapshot.data!.length,
-                        padding: const EdgeInsets.all(10),
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: count,
-                          mainAxisSpacing: 8,
-                          crossAxisSpacing: 1,
-                          mainAxisExtent: mainAxisExtent,
-                        ),
-                        itemBuilder: (context, index) {
-                          return CardItem(
-                            data: snapshot.data![index],
-                            imageSize: sizeImage,
-                          );
-                        },
-                      );
-                    },
-                  );
-                } else {
-                  return Center(
-                    child: Text(
-                      'not have item',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                      ),
-                    ),
-                  );
-                }
-              } else if (!snapshot.hasData) {
+              if (!snapshot.hasData) {
                 return const Center(
                   child: CircularProgressIndicator.adaptive(),
+                );
+              } else if (snapshot.data!.isEmpty) {
+                return Center(
+                  child: Text(
+                    'item kosong',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  ),
+                );
+              }
+              if (snapshot.hasData) {
+                return LayoutBuilder(
+                  builder: (context, constraints) {
+                    int count;
+                    double mainAxisExtent;
+                    print(constraints.maxWidth);
+
+                    if (constraints.maxWidth < 480) {
+                      count = 2;
+                      mainAxisExtent = constraints.maxWidth * 0.67;
+                    } else if (constraints.maxWidth < 700) {
+                      count = 3;
+                      mainAxisExtent = constraints.maxWidth * 0.5;
+                    } else if (constraints.maxWidth < 900) {
+                      count = 4;
+                      mainAxisExtent = constraints.maxWidth * 0.35;
+                    } else if (constraints.maxWidth < 1000) {
+                      count = 5;
+                      mainAxisExtent = constraints.maxWidth * 0.3;
+                    } else {
+                      count = 6;
+                      mainAxisExtent = constraints.maxWidth * 0.25;
+                    }
+
+                    double sizeImage = constraints.maxWidth / count;
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      padding: const EdgeInsets.all(10),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: count,
+                        mainAxisSpacing: 8,
+                        crossAxisSpacing: 1,
+                        mainAxisExtent: mainAxisExtent,
+                      ),
+                      itemBuilder: (context, index) {
+                        return CardItem(
+                          data: snapshot.data![index],
+                          imageSize: sizeImage,
+                          callback: () {
+                            setState(() {
+                              wsHelper.getDataAllCollectionOnce();
+                            });
+                          },
+                        );
+                      },
+                    );
+                  },
                 );
               } else {
                 return Center(
                   child: Text(
-                    'is not found ${snapshot.error}',
+                    'error on server ${snapshot.error}',
                     style: TextStyle(
                       color: Theme.of(context).colorScheme.onPrimary,
                     ),
