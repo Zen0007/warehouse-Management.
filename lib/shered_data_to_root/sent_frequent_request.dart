0704dd030699dedb477de64e3d55bb14820c2d0a
Zen0007 <zmr8388@gmail.com>
2025-02-02 18:03:53 +0700

    sent frequent request in statet menegement

---
+++ lib/shered_data_to_root/sent_frequent_request.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'package:werehouse_inventory/shered_data_to_root/websocket_helper.dart';
+
+class FrequentRequestHandler {
+  FrequentRequestHandler(this.wsHelper);
+  final WebsocketHelper wsHelper;
+  Timer? timer;
+
+  void frequentRequestCollectionAdmin() {
+    timer = Timer.periodic(
+      Duration(seconds: 10),
+      (_) {
+        wsHelper.getDataAllCollection();
+      },
+    );
+  }
+}
