d99b5e016851724a9c8b48218b1f8c6a3f39a038
Asta-2007 <docker624@gmail.com>
2025-02-13 14:32:52 +0700

    break card to one task

---
+++ lib/card/card_item_user.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:werehouse_inventory/data%20type/index.dart';
+import 'package:werehouse_inventory/shered_data_to_root/shared_preferences.dart';
+import 'package:werehouse_inventory/shered_data_to_root/websocket_helper.dart';
+
+class CardItemUser extends StatelessWidget {
+  const CardItemUser({
+    super.key,
+    required this.data,
+    required this.imageSize,
+  });
+
+  final Index data;
+  final double imageSize;
+
+  Future<void> store(
+      String category, String index, String name, String label) async {
+    await StoredUserChoice().addNewMapToSharedPreferences(
+      {
+        "category": category,
+        "index": index,
+        "nameItem": name,
+        "label": label,
+      },
+    );
+    final dataShow = await StoredUserChoice().getListFromSharedPreferences();
+
+    debugPrint("$dataShow data carditem");
+  }
+
+  Future<dynamic> messages(BuildContext context, bool isMessage,
+      String response, Color color, Color backgroundColor) {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog.adaptive(
+        backgroundColor: backgroundColor,
+        title: Text(
+          isMessage ? "MESSAGE" : "WARNING",
+          style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        content: Text(
+          response,
+          style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        actions: [
+          OutlinedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Yes",
+              style: TextStyle(
+                color: color,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void updateStatus(BuildContext context, WebsocketHelper wsHelper) async {
+    await for (var status in wsHelper.streamControllerAll.stream) {
+      if (status['endpoint'] == "UPDATESTATUSITEM") {
+        if (status.containsKey('message')) {
+          if (!context.mounted) return;
+
+          messages(
+            context,
+            true,
+            status['message'],
+            Theme.of(context).colorScheme.onSecondary,
+            Theme.of(context).colorScheme.surface,
+          );
+
+          return;
+        } else {
+          if (!context.mounted) return;
+          messages(
+            context,
+            false,
+            status['warning'],
+            Theme.of(context).colorScheme.onError,
+            Theme.of(context).colorScheme.error,
+          );
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      key: ValueKey(data.index),
+      color: Theme.of(context).colorScheme.secondary,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (data.image.isNotEmpty)
+            Container(
+              margin: const EdgeInsets.only(
+                top: 10,
+                bottom: 8,
+              ),
+              decoration: BoxDecoration(
+                boxShadow: const [
+                  BoxShadow(
+                    // color: Theme.of(context).colorScheme.primary,
+                    blurRadius: 10,
+                    offset: Offset(0, 10),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.memory(
+                  data.image,
+                  height: imageSize * 0.65,
+                  width: imageSize * 0.8,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+          else
+            Container(
+              height: imageSize * 0.65,
+              width: imageSize * 0.8,
+              margin: const EdgeInsets.only(
+                top: 10,
+                bottom: 8,
+              ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Icon(
+                  Icons.image_not_supported_outlined,
+                  size: 50,
+                ),
+              ),
+            ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "name :",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                data.name,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.w400,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "label   :",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                data.label,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "status :",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                data.status,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 5, right: 6),
+            child: Consumer<WebsocketHelper>(
+              builder: (context, wsHelper, child) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        wsHelper.sendMessage({
+                          'endpoint': 'updateStatusItem',
+                          'data': {
+                            'category': data.category,
+                            'index': data.index,
+                          }
+                        });
+                        store(data.category, data.index, data.name, data.label);
+                        updateStatus(context, wsHelper);
+                      },
+                      style: IconButton.styleFrom(
+                        backgroundColor:
+                            Theme.of(context).colorScheme.secondary,
+                      ),
+                      icon: Icon(
+                        Icons.bookmark_add_outlined,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
