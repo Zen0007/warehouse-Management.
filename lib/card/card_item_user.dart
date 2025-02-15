48267b832192b3e1be43892ac8e09fd1c5b835c7
Asta-2007 <docker624@gmail.com>
2025-02-15 14:02:48 +0700

    fix positioned button add list borrow item

--- lib/card/card_item_user.dart
+++ lib/card/card_item_user.dart
@@ -102,125 +102,129 @@ class CardItemUser extends StatelessWidget {
     return Card(
       key: ValueKey(data.index),
       color: Theme.of(context).colorScheme.secondary,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Stack(
         children: [
-          if (data.image.isNotEmpty)
-            Container(
-              margin: const EdgeInsets.only(
-                top: 10,
-                bottom: 8,
-              ),
-              decoration: BoxDecoration(
-                boxShadow: const [
-                  BoxShadow(
-                    // color: Theme.of(context).colorScheme.primary,
-                    blurRadius: 10,
-                    offset: Offset(0, 10),
+          Column(
+            children: [
+              if (data.image.isNotEmpty)
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    bottom: 8,
                   ),
-                ],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.memory(
-                  data.image,
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        // color: Theme.of(context).colorScheme.primary,
+                        blurRadius: 10,
+                        offset: Offset(0, 10),
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.memory(
+                      data.image,
+                      height: imageSize * 0.65,
+                      width: imageSize * 0.8,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              else
+                Container(
                   height: imageSize * 0.65,
                   width: imageSize * 0.8,
-                  fit: BoxFit.cover,
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    bottom: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.image_not_supported_outlined,
+                      size: 50,
+                    ),
+                  ),
                 ),
-              ),
-            )
-          else
-            Container(
-              height: imageSize * 0.65,
-              width: imageSize * 0.8,
-              margin: const EdgeInsets.only(
-                top: 10,
-                bottom: 8,
-              ),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surface,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.image_not_supported_outlined,
-                  size: 50,
-                ),
-              ),
-            ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                "name :",
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
               const SizedBox(
-                width: 4,
+                height: 10,
               ),
-              Text(
-                data.name,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontWeight: FontWeight.w400,
-                ),
-              )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "name :",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    data.name,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "label   :",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    data.label,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "status :",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    data.status,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                "label   :",
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                width: 4,
-              ),
-              Text(
-                data.label,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                "status :",
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                width: 4,
-              ),
-              Text(
-                data.status,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimary,
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 5, right: 6),
+          Positioned(
+            right: 5,
+            bottom: 10,
             child: Consumer<WebsocketHelper>(
               builder: (context, wsHelper, child) {
                 return Row(
@@ -244,6 +248,7 @@ class CardItemUser extends StatelessWidget {
                       ),
                       icon: Icon(
                         Icons.bookmark_add_outlined,
+                        size: 30,
                       ),
                     ),
                   ],
