ad2819382fd1ea6ee5707aa193362c5aa45dab15
Asta-2007 <docker624@gmail.com>
2025-02-15 14:03:30 +0700

    fix positioned button

--- lib/card/card_pending.dart
+++ lib/card/card_pending.dart
@@ -15,139 +15,166 @@ class CardPending extends StatelessWidget {
     return Card(
       key: ValueKey(data.time),
       color: Theme.of(context).colorScheme.secondary,
-      child: Column(
+      child: Stack(
         children: [
-          Container(
-            margin: const EdgeInsets.only(
-              top: 10,
-              bottom: 8,
-            ),
-            decoration: BoxDecoration(
-              boxShadow: const [
-                BoxShadow(
-                  // color: Theme.of(context).colorScheme.primary,
-                  blurRadius: 10,
-                  offset: Offset(0, 10),
-                ),
-              ],
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.memory(
-                data.imageUser,
-                height: imageSize * 0.65,
-                width: imageSize * 0.8,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Row(
+          Column(
             children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      "name",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontWeight: FontWeight.bold,
+              if (data.imageUser.isNotEmpty)
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    bottom: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        // color: Theme.of(context).colorScheme.primary,
+                        blurRadius: 10,
+                        offset: Offset(0, 10),
                       ),
+                    ],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.memory(
+                      data.imageUser,
+                      height: imageSize * 0.65,
+                      width: imageSize * 0.8,
+                      fit: BoxFit.cover,
                     ),
-                    Text(
-                      "kelas",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                )
+              else
+                Container(
+                  height: imageSize * 0.65,
+                  width: imageSize * 0.8,
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    bottom: 8,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.image_not_supported_outlined,
+                      size: 50,
                     ),
-                    Text(
-                      "guru",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "nisn",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
+              const SizedBox(
+                height: 15,
               ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      "${data.nameUser}",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontWeight: FontWeight.w800,
-                      ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "name",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "kelas",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "guru",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "nisn",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      "${data.nameTeacher}",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontWeight: FontWeight.w800,
-                      ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "${data.nameUser}",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          "${data.nameTeacher}",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        Text(
+                          "${data.classUser}",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        Text(
+                          "${data.nisn}",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      "${data.classUser}",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    Text(
-                      "${data.nisn}",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.onPrimary,
-                      ),
-                    ),
-                  ],
-                ),
-              )
+                  )
+                ],
+              ),
             ],
           ),
-          Spacer(),
-          Consumer<WebsocketHelper>(
-            builder: (context, wsHelper, child) {
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 5, right: 6),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    OutlinedButton(
-                      onPressed: () {
-                        warning(context, wsHelper);
-                        print("press ");
-                      },
-                      style: IconButton.styleFrom(
-                        backgroundColor:
-                            Theme.of(context).colorScheme.secondary,
-                      ),
-                      child: Text(
-                        "Yes",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onError,
-                          fontWeight: FontWeight.w700,
+          Positioned(
+            right: 5,
+            bottom: 10,
+            child: Consumer<WebsocketHelper>(
+              builder: (context, wsHelper, child) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 5, right: 6),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      OutlinedButton(
+                        onPressed: () {
+                          warning(context, wsHelper);
+                          print("press ");
+                        },
+                        style: IconButton.styleFrom(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary,
+                        ),
+                        child: Text(
+                          "Yes",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onError,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
